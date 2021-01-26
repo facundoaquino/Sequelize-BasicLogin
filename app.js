86e7a735afdb9f1f3b2a54421212e3753561cc42
Facundo Aquino <facun90@live.com>
2021-01-26 18:03:57 -0300

    changes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mainRouter = require('./routes/mainRouter');
 const app = express()
-const MemoryStore =require('memorystore')
+// const MemoryStore =require('memorystore')
+
+
 const session = require('express-session');
+
+var MySQLStore = require('express-mysql-session')(session);
 
 /*---------------------- global middleware ---------------------*/
 const authMiddleware = require('./middlewares/authmiddleware');
 
+
+var options = {
+	host: 'localhost',
+	port: 3306,
+	user: 'b13b87e577c859',
+	password: 'pase9b67217sword',
+	database: 'heroku_c4c796f2d40c24a'
+};
+
+var sessionStore = new MySQLStore(options);
 
 
 //  defined port
@@ -15,7 +29,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 // app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true}))
-app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true ,store: new MemoryStore()}))
+app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true ,store: sessionStore}))
 app.use(authMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
