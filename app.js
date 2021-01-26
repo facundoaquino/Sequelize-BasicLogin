284a3c207b5a58b597aa97b77b8516b74807ecbe
Facundo Aquino <facun90@live.com>
2021-01-26 20:33:55 -0300

    session setings

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mainRouter = require('./routes/mainRouter');
 const app = express()
-// const MemoryStore =require('memorystore')
+ 
 
 
 const session = require('express-session');
@@ -11,16 +11,24 @@ var MySQLStore = require('express-mysql-session')(session);
 /*---------------------- global middleware ---------------------*/
 const authMiddleware = require('./middlewares/authmiddleware');
 
+if(process.env.NODE_ENV ){
 
-// var options = {
-// 	host: 'us-cdbr-east-03.cleardb.com',
-// 	port: 3306,
-// 	user: 'b13b87e577c859',
-// 	password: 'e9b67217',
-// 	database: 'heroku_c4c796f2d40c24a'
-// };
+    var sessionStore = new MySQLStore(options);
 
-// var sessionStore = new MySQLStore(options);
+    var options = {
+        host: 'us-cdbr-east-03.cleardb.com',
+        port: 3306,
+        user: 'b13b87e577c859',
+        password: 'e9b67217',
+        database: 'heroku_c4c796f2d40c24a'
+    };
+
+    app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true ,store: sessionStore}))
+}else{
+
+    app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true}))
+}
+
 
 
 //  defined port
@@ -29,8 +37,6 @@ const port = process.env.PORT || 3000
 
 console.log(process.env.NODE_ENV);
 app.use(express.static('public'))
-app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true}))
-// app.use(session({secret:'TheSecretSession',resave:false,saveUninitialized:true ,store: sessionStore}))
 app.use(authMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
